a1edf103fd2c25b775eaf78d62657fa3e3db36f5
Elizabeth Burkholder <e-burkholder@onu.edu>
2023-10-25 23:10:39 -0400

    Completed romanCharValue function.

--- p8-3/Source.cpp
+++ p8-3/Source.cpp
@@ -1,1 +1,35 @@
-//p8-3
+#include <iostream>
+#include <string>
+using namespace std;
+
+int romanCharValue(char r);
+
+int main() {
+	char romanNum;
+	int number;
+	cout << "Enter a character to test part 1: ";
+	cin >> romanNum;
+	number = romanCharValue(romanNum);
+	cout << "The value of " << romanNum << " is " << number;
+}
+
+int romanCharValue(char r) {
+	int value;
+	switch (r) {
+	case 'I':
+		value = 1; break;
+	case 'V':
+		value = 5; break;
+	case 'X':
+		value = 10; break;
+	case 'L':
+		value = 50; break;
+	case 'C':
+		value = 100; break;
+	case 'D':
+		value = 500; break;
+	case 'M':
+		value = 1000; break;
+	}
+	return value;
+}
