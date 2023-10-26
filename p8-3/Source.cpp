5a726915f8dccc323bef823e300708d0429ab0b6
Elizabeth Burkholder <e-burkholder@onu.edu>
2023-10-26 01:14:42 -0400

    completed convertRomanToInt function.

--- p8-3/Source.cpp
+++ p8-3/Source.cpp
@@ -3,33 +3,53 @@
 using namespace std;
 
 int romanCharValue(char r);
+int convertRomanToInt(string s);
 
 int main() {
-	char romanNum;
-	int number;
-	cout << "Enter a character to test part 1: ";
-	cin >> romanNum;
-	number = romanCharValue(romanNum);
-	cout << "The value of " << romanNum << " is " << number;
+	string romanNum;
+	cout << "Enter a roman number: ";
+	getline(cin, romanNum);
+	cout << romanNum << " = " << convertRomanToInt(romanNum);
 }
 
 int romanCharValue(char r) {
 	int value;
 	switch (r) {
 	case 'I':
-		value = 1; break;
+		value = 1; 	return value;
 	case 'V':
-		value = 5; break;
+		value = 5; 	return value;
 	case 'X':
-		value = 10; break;
+		value = 10;	return value;
 	case 'L':
-		value = 50; break;
+		value = 50;	return value;
 	case 'C':
-		value = 100; break;
+		value = 100; return value;
 	case 'D':
-		value = 500; break;
+		value = 500; return value;
 	case 'M':
-		value = 1000; break;
+		value = 1000; return value;
+	default:
+		return 0;
 	}
-	return value;
+}
+
+int convertRomanToInt(string s) {
+	int total = 0;
+	while (s!="") {
+		char firstChar = s[0];
+		char secChar = s[1];
+		int a1 = romanCharValue(firstChar);
+		int a2 = romanCharValue(secChar);
+
+		if (a1 >= a2 || s.length() == 1) {
+			total += a1;
+			s = s.substr(1);
+		}
+		else {
+			total += a2 - a1;
+			s = s.substr(2);
+		}
+	}
+		return total;
 }
